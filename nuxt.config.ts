06d2227d343d5572ad3932f56b14b45ac2e7d202
snowman12320 <snowman12320@gmail.com>
2024-01-31 22:04:57 +0800

    Refactor Nuxt configuration and remove unused script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,21 +2,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  typescript: {
-    typeCheck: false,
-  },
-
   modules: ['@vueuse/nuxt'],
   imports: {
     autoImport: true,
     dirs: ['composables/**'],
-  },
-  css: ['./assets/css/main.sass'],
-  build: {
-    loaders: {
-      scss: {
-        additionalData: `@import "./assets/css/main.sass";`,
-      },
-    },
   },
 });
